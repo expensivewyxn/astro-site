a9ead106a829ae1563f8f6456618993e4bbd7e7b
Ryan <ryanhin@hotmail.com>
2022-08-23 18:07:50 +0800

    Added onScroll colored navbar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,36 @@
+import styles from "../styles/components/Navbar.module.scss"
+import { motion, useScroll } from "framer-motion"
+import { useEffect, useState } from "react"
+
+export default function Navbar() {
+  const { scrollY } = useScroll()
+  const [scrolled, setScrolled] = useState(false)
+
+  useEffect(() => {
+    return scrollY.onChange((latest) => {
+      if (latest > 100) {
+        setScrolled(true)
+        console.log(latest)
+      } else {
+        setScrolled(false)
+      }
+    })
+  }, [])
+
+  return (
+    <motion.nav className={`${styles.navbar} ${scrolled && styles.darkbg}`}>
+      <div>
+        <h2>Ryan Leung</h2>
+        <ul>
+          <li>
+            <a href="/">Home</a>
+          </li>
+          <li>
+            <a href="/about">About</a>
+          </li>
+          <li>Contact</li>
+        </ul>
+      </div>
+    </motion.nav>
+  )
+}
